5173633adbbb7e6f848435b0d07f55f6b9b917ce
eric <ericverhoeff91@hotmail.com>
2019-04-10 14:55:09 +0200

    Added multi-core gdal2tiles, included ortho clippings

--- clip_ortho_2_plot.py
+++ clip_ortho_2_plot.py
@@ -11,7 +11,7 @@ import rasterio.mask
 import shapely
 import numpy as np
 import json
-from geoalchemy2.shape import to_shape 
+from geoalchemy2.shape import to_shape
 import geopandas as gpd
 from fiona.crs import from_epsg
 import gdal
@@ -19,27 +19,10 @@ import gdal
 config_file_path = r'C:\Users\VanBoven\MijnVanBoven\config.json'
 port = 5432
 
-with open(config_file_path) as config_file: 
-    config = json.load(config_file)
-    
-host=config.get("DB_IP")
-db=config.get("DB_NAME")
-user=config.get("DB_USER")
-password=config.get("DB_PASSWORD")
-
-meta, con = connect(user, password, db, host, port=5432)
-
-ortho_ready_inbox = r'C:\Users\VanBoven\Documents\100 Ortho Inbox\ready'
-
-for file in os.listdir(ortho_ready_inbox):
-    if file.endswith('.tif'):
-            this_customer_name,this_plot_name,this_date = file.split('-')
-            this_date = this_date.split('.')[0]
-            clip_ortho2plot(this_plot_name, con, meta, ortho_ready_inbox, file)
 
 def clip_ortho2plot(this_plot_name, con, meta, ortho_ready_inbox, file):
     with rasterio.open(os.path.join(ortho_ready_inbox, file)) as src:
-        geometry = to_shape(get_plot_shape(this_plot_name, con, meta))   
+        geometry = to_shape(get_plot_shape(this_plot_name, meta, con))
         geo = gpd.GeoDataFrame({'geometry': geometry}, index=[0], crs=from_epsg(4326))
         coords = getFeatures(geo)
         out_image, out_transform = rasterio.mask.mask(src, coords, crop=True)
@@ -47,10 +30,11 @@ def clip_ortho2plot(this_plot_name, con, meta, ortho_ready_inbox, file):
     out_meta.update({"driver": "GTiff",
                              "height": out_image.shape[1],
                              "width": out_image.shape[2],
-                             "transform": out_transform})            
-    with rasterio.open(os.path.join(ortho_ready_inbox, str(file[:-4])+"_clipped.tif"), "w", **out_meta, BIGTIFF='YES') as dest:#, compress="JPEG"
+                             "transform": out_transform})
+    with rasterio.open(os.path.join(ortho_ready_inbox, str(file[:-4])+"_clipped.tif"), \
+    "w", **out_meta, BIGTIFF='YES',NUM_THREADS='ALL_CPUS',COMPRESS='LZW') as dest:
         dest.write(out_image)
-        
+
 def getFeatures(gdf):
     """Function to parse features from GeoDataFrame in such a manner that rasterio wants them"""
     return [json.loads(gdf.to_json())['features'][0]['geometry']]
--- process_from_inbox.py
+++ process_from_inbox.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 """
 @author: Kaz
@@ -5,16 +6,18 @@
 import os
 from pathlib import Path
 import time
-import datetime
 import sys
 #import gdal2tiles
 import shutil
+import gdal
 import pandas as pd
 import subprocess
 import paramiko
-from gdal2tilesp import gdal2tilesp
+from clip_ortho_2_plot import clip_ortho2plot
 import logging
 from vanbovendatabase.postgres_lib import *
+import datetime
+
 
 
 ## FUNCTIONS ##
@@ -63,9 +66,10 @@ def cmd_and_wait(ssh,command):
 
 ## CONFIG SECTION ##
 inbox = r'C:\Users\VanBoven\Documents\100 Ortho Inbox\ready' #folder where all orthos are stored
-ortho_archive_destination = r'C:\Users\VanBoven\Documents\100 Ortho Inbox\ready' #Folder where orthos are archived (gdrive)
+ortho_archive_destination = r'E:\VanBovenDrive\VanBoven MT\Archive' #Folder where orthos are archived (gdrive)
 pem_path= r"C:\Users\VanBoven\Documents\SSH\VanBovenAdmin.pem"
 gdal2tilesp_location = os.path.join(Path.cwd(),'gdal2tilesp','gdal2tilesp.py') #location of gdal2tiles.py folder
+
 
 with open('postgis_config.json') as config_file:
     config = json.load(config_file)
@@ -74,13 +78,7 @@ DB_USER = config['DB_USER']
 DB_PASSWORD = config['DB_PASSWORD']
 DB_IP = config['DB_IP']
 
-
-#DEV LINES:
-inbox = '/Users/kazv/Desktop/inbox/ready'
-pem_path = '/Users/kazv/.ssh/VanBovenAdmin.pem'
-ortho_archive_destination = '/Users/kazv/Desktop/ortho_archive/'
-
-
+con,meta = connect(DB_USER, DB_PASSWORD, DB_NAME, host=DB_IP)
 #initialize
 orthos = []
 tif_count = 0
@@ -125,10 +123,11 @@ ortho_que = sorted(orthos, key=lambda k: k['date'])
 tile_output_base_dir = os.path.join(inbox,'temp_tiles')
 if not(os.path.isdir(tile_output_base_dir)):
     os.mkdir(tile_output_base_dir)
-
 #Loop trough all available tifs and tile them.
+
 for ortho in ortho_que:
     start_ortho_time = time.time()
+    print('Started with {} at {}'.format(ortho,start_ortho_time))
 
     #Read dict
     customer_name = ortho['customer_name']
@@ -138,31 +137,41 @@ for ortho in ortho_que:
     logging.info('Starting processing of {}\n'.format(filename))
 
     #Check for possible duplicate work:
-    ortho_archive_target = os.path.join(ortho_archive_destination,customer_name,plot_name,date,'orthomosaic')
+    ortho_archive_target = os.path.join(ortho_archive_destination,customer_name,plot_name,date,'Orthomosaic')
     if os.path.isfile(ortho_archive_target): #This needs to be tested for the 'break'
         #File already exists, quiting early and logging duplicate
         logging.info('    Ortho is already present in archive. Exited after {} seconds\n'\
         .format(round(time.time() - start_ortho_time)))
         break
 
-
-    logging.info('    Tiling {}...\n'.format(filename))
-
-    #Identify and create file locations
-    input_file = os.path.join(inbox,filename)
-    output_file = os.path.join(tile_output_base_dir,filename.split('.')[0])
-
-    #DEBUGGIN: Skip if already tiled
-    if os.path.isdir(output_file):
-        newtile = False
-    else:
-        newtile = True
-        os.mkdir(output_file)
+    #clip ortho to plot shape:
+    logging.info('    Clipping {}...\n'.format(filename))
+    start_clip_time = time.time()
+    clip_ortho2plot(plot_name, con, meta, inbox,filename)
+    end_clip_time = time.time()
+    clip_duration = round((end_clip_time-start_clip_time)/60)
+    logging.info('    Clipped in {} minutes...\n'.format(clip_duration))
+    filename_clipped = filename.split('.')[0]+'_clipped.'+filename.split('.')[-1]
 
     #Start tiling
     logging.info('Start tiling proces for {}\n'.format(filename))
     start_tiling_time = time.time()
-    batcmd =gdal2tilesp_location+ ' "' + str(input_file) + '"' + ' "' + str(output_file) + '"'+ ' -z 16-22 -w none -o tms'
+    #Identify and create file locations
+    input_file = os.path.join(inbox,filename_clipped)
+    output_folder = os.path.join(tile_output_base_dir,filename.split('.')[0])
+    #DEBUGGIN: Skip if already tiled
+    if os.path.isdir(output_folder):
+        newtile = False
+    else:
+        newtile = True
+    os.mkdir(output_folder)
+    batcmd ='python gdal2tiles.py' + ' "' + str(input_file) + '"' + ' "' + str(output_folder) + '"'+ ' -z 16-23 -w none --processes 16'
+
+    #Would be great if we can use the direct python funciton. This requires either building an options args element, or manually fixing gdal2tiles.py
+    #argv= gdal.GeneralCmdLineProcessor(['"'+str(input_file)+'"','"'+str(output_folder)+'"','--processes' ,'14' ,'-z', '16-23', '-w' ,'none'])
+    #input_file, output_folder, options = process_args(argv[1:])
+    #multi_threaded_tiling(input_file, output_folder, gdal2tiles_options)
+
     if newtile:
         os.system(batcmd)
 
@@ -172,12 +181,12 @@ for ortho in ortho_que:
 
     #Zip ortho folders
     #provide make_archive with the location of the just created tile folder
-    logging.info('    Zipping tiles to {}...\n'.format(output_file))
-    local_zipfile = shutil.make_archive(output_file, 'zip', output_file)
+    logging.info('    Zipping tiles to {}...\n'.format(output_folder))
+    local_zipfile = shutil.make_archive(output_folder, 'zip', output_folder)
 
     #Clean up by removing the original folder. rmtree permanently removes them - be warned.
     logging.info('    Removing original tiles \n')
-    shutil.rmtree(output_file)
+    shutil.rmtree(output_folder)
 
     ## Uploading ##
     logging.info('    Preparing to upload.\n')
@@ -213,7 +222,7 @@ for ortho in ortho_que:
 
 
     #unzip file to the right dir and remove archive
-    remote_unzip_location = '/home/ubuntu/media/data/' + customer_name + '/' +plot_name +'/' +  date
+    remote_unzip_location = '/home/ubuntu/media/data/' + customer_name + '/' + plot_name +'/' +  date
     #Specify bash ssh commands
     command_unzip = 'unzip ' + "'"+ full_remote_zip_path + "'" + ' -d ' + "'" + remote_unzip_location + "'"
     command_removezip = 'rm ' + "'"+ full_remote_zip_path + "'"
@@ -234,12 +243,17 @@ for ortho in ortho_que:
     #Move orthomosaic to correct folder
     if not(os.path.isdir(ortho_archive_target)):
         os.makedirs(ortho_archive_target)
-    shutil.move(input_file,os.path.join(ortho_archive_target,filename))
+    shutil.move(input_file,os.path.join(ortho_archive_target,filename_clipped))
+    os.remove(os.path.join(inbox,filename))
 
     end_ortho_time = time.time()
     logging.info('    Finished processing {} in {} minutes\n \n'.\
     format(filename,round((start_ortho_time-end_ortho_time)/60)))
 
-    con,meta = connect(DB_USER, DB_PASSWORD, DB_NAME, host=DB_IP)
     insert_new_scan(date, plot_name, meta, con)
-    logging.info("Added scan {} to plot {}".format(scan_id,plot_name))
+    logging.info("Added scan {} to plot {}".format(date,plot_name))
+
+
+con.connect().close()
+print('Reached end of script')
+logging.info('Reached end of script')
